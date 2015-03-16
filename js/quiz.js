d5c265c159aa550a15ebd3c8527bc79c6d312c10
Rizwan Ali <rizcube@gmail.com>
2015-03-16 11:46:51 +0000

    it knows the correct and incorrect questions

--- js/quiz.js
+++ js/quiz.js
@@ -3,12 +3,36 @@ function print(message) {
 }
 
 var questions= [
-  ['question 1', 'answer 1'],
-  ['question 2', 'answer 2'],
-  ['question 3', 'answer 3'],
-  ['question 4', 'answer 4']
+  ['What is question 1?', 'question 1'],
+  ['What is questions 2?', 'question 2'],
+  ['What is question 3?', 'question 3'],
+  ['What is question 4?', 'question 4']
 ];
 
-for (i=0; i<questions.length; i+=1) {
-  alert(questions[i]);
+correct_ans = 0;
+incorrect_ans = 0;
+correct_given_ans= [[]];
+incorrect_given_ans = [[]];
+
+for (i=0; i<questions.length; i+=1){
+  user_ans = prompt(questions[i][0]);
+  if (user_ans === questions[i][1]) {
+    correct_given_ans.push(user_ans)
+    // document.write('<p>' + correct_given_ans + '</p>');
+    correct_ans += 1
+    // document.write("------------------------");
+    // document.write('correct:' + correct_ans);
+  } else if (user_ans !== [i][1]) {
+    incorrect_ans += 1
+    incorrect_given_ans.push([i][0])
+    document.write(incorrect_given_ans);
+    incorrect_given_ans.push(user_ans);
+    // document.write('incorrect:' + incorrect_ans);
+
+  }
 }
+document.write('<p>' + 'You got the following questions correct:' + '<p>'+ correct_given_ans + '</p>' + '</p>');
+document.write('<p>' + 'You got the following questions incorrect:' + '<p>'+ incorrect_given_ans + '</p>' + '</p>');
+
+document.write('<p>' + 'You got  ' + correct_ans + '  correct'+ '</p>' );
+document.write('<p>' + 'You got  ' + incorrect_ans + '  incorrect' + '</p>');
