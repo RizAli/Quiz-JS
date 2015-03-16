47c36c9ca344e68d052e8768996986bd6b7fa0d4
Rizwan Ali <rizcube@gmail.com>
2015-03-16 15:23:26 +0000

    revision

--- js/quiz.js
+++ js/quiz.js
@@ -21,38 +21,3 @@ var html;
 var correct = [];
 var wrong =[];
 
-function print(message) {
-  var outputDiv = document.getElementById('output');  //DOM object
-  outputDiv.innerHTML = message;
-}
-
-function buildlist(arr) {
-  var listHTML = '<ol>';
-   for (var i=0; i <arr.length; i +=1) {
-    listHTML += '<li>' + arr[i] + '</li>';
-   }
-listHTML += '</ol>';
-    return listHTML;
-}
-
-
-
-for (var i=0; i <questions.length; i+=1) {
-question = questions[i].question;
-answer = questions[i].answer;
-response = parseInt(prompt(question));
-  if (response === answer) {
-    correctAnswers +=1;
-    correct.push(question);
-  }  else {
-    wrong.push(question);
-  }
-}
-
-html = "You got " + correctAnswers + " question(s) right.";
-html += '<h2> You got these questions correct: </h2>';
-html += buildlist(correct);
-html += '<h2> You got these questions worng: </h2>';
-html += buildlist(wrong);
-print(html);
-
