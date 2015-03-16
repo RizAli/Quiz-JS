1205bba0d577a59e273af5508a1dc2c545f4830e
Rizwan Ali <rizcube@gmail.com>
2015-03-16 17:24:10 +0000

    it can tell how many and which question user got right or wrong

--- js/quiz.js
+++ js/quiz.js
@@ -6,6 +6,7 @@ var questions= [
 ];
 
 var correctAnswers = 0;
+var wrongAnswers = 0;
 var question;
 var answer;
 var response;
@@ -13,9 +14,36 @@ var html;
 var correct = [];
 var wrong =[];
 
+var print = function(message){
+  document.write(message);
+};
+
+
 
 for (i = 0; i<questions.length;  i=i+1){
   question = questions[i][0];
   answer = questions[i][1];
   response = parseInt(prompt(question));
+
+  if (response === answer) {
+    correctAnswers +=1;
+    correct.push(question);
+
+  } else if (response !== answer){
+    wrongAnswers +=1;
+    wrong.push(question);
+
+  }
 }
+
+
+html_correct = "<p> You got " + correctAnswers + " question(s) right </p>";
+html_incorrect = "<p> You got " + wrongAnswers + " questions(s) wrong</p>";
+
+html_correct_questions = "<p> You got the following question(s) correct</p>" + correct
+html_wrong_responses = "<p> You got the following question(s) wrong </p>" + wrong
+
+print(html_correct);
+print(html_incorrect);
+print(html_correct_questions);
+print(html_wrong_responses);
